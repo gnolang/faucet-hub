15079540db695bce4af482c31911e7563ef0d8cd
Alexis Colin <alexis@jaunebleu.co>
2025-04-14 15:03:45 +0900

    fix: animation and default captcha key (#43)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { VueRecaptcha } from 'vue-recaptcha'
+import { faucetDetailPlugin } from '@/stores/faucetDetail'
 
 import './style.css'
 import App from './App.vue'
 
 const pinia = createPinia()
+pinia.use(faucetDetailPlugin)
+
 const app = createApp(App)
 
 app.use(pinia)
