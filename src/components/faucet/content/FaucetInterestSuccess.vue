5ad47804101a405de458d5ccff4e93b9d405154b
Clockwork <alex@clockwork.gr>
2026-04-02 13:39:13 +0300

    text: Update src/components/faucet/content/FaucetInterestSuccess.vue
    Co-authored-by: nemanjantic <nemanja@newtendermint.org>

--- src/components/faucet/content/FaucetInterestSuccess.vue
+++ src/components/faucet/content/FaucetInterestSuccess.vue
@@ -3,7 +3,7 @@
     <h2 class="text-500 mt-8">Application submitted!</h2>
     <div class="js-faucetsuccessdetail h-0 overflow-hidden">
       <p class="font-light text-grey-100 mt-6">
-        Your application for Beta gno.land access is now under review. You will be contacted via the email you provided once your application has been approved and funds have been allocated.
+        Your application for Beta Gno.land funds is now under review. You will be contacted via the email you provided once your application has been approved and funds have been allocated.
       </p>
 
       <Button text="Done" class="w-full mt-12" @click.prevent="() => $emit('doneFaucet')" />
